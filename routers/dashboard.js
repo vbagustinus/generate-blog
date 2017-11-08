e5a9d27874ee51cc0d2cdc44a9e7759237166810
aditReza <lete.note3@gmail.com>
2017-11-08 14:21:05 +0700

    router dashboard

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -19,6 +19,7 @@ router.get('/post/:id', checkLogin ,function(req,res){
     }
   })
   .then(dataPosts => {
+    // console.log(dataPosts);
     res.render('dashboardPost',{dataPosts:dataPosts,username:req.session.username, user_id:req.session.user_id})
   })
   .catch( err => {
@@ -31,15 +32,31 @@ router.get('/post/:id', checkLogin ,function(req,res){
 //----------------------
 
 router.get('/post/edit/:id',function(req,res){
-  models.Post.findById(
-  {
-    where: req.params.id
-  })
+  models.Post.findById(req.params.id)
   .then(dataPost=>{
+    // res.send(dataPost);
       res.render('editPost',{dataPost:dataPost, username:req.session.username, user_id:req.session.user_id})
   })
   .catch(err=>{
     res.send(err)
+  })
+})
+
+router.post('/post/edit/:id', function(req,res){
+  models.post.update({
+    title : req.body.title,
+    article : req.body.article,
+    date_publish: new Date(),
+    link: setlink,
+    UserId: req.session.user_id
+  },{
+    where : {
+      id : req.params.id
+    }
+  }).then(function(){
+    res.redirect('../dashboard')
+  }).catch(function(err){
+    console.log(err);
   })
 })
 
@@ -66,7 +83,6 @@ router.post('/addpost', function(req,res){
     title: input.title,
     article: input.article,
     date_publish: new Date(),
-    category_name: input.category_name,
     link: setlink,
     UserId: req.session.user_id
   })
@@ -90,6 +106,23 @@ router.post('/addpost', function(req,res){
   .catch(err=>{
     console.log(err);
     res.send(err)
+  })
+})
+
+
+//----------------------
+// DELETE
+//----------------------
+
+router.get('/delete/:id', function(req,res){
+  models.Post.destroy({
+    where : {
+      id : req.params.id
+    }
+  }).then(function(){
+    res.redirect('../dashboard')
+  }).catch(function(err){
+    console.log(err);
   })
 })
 
